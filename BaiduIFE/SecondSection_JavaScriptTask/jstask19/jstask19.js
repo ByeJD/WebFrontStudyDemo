e1df95659e4e48b2126e2065da99f99777bd7f83
ByeHdu <15988855324@163.com>
2017-02-15 22:57:39 +0800

    bubblesort

--- BaiduIFE/SecondSection_JavaScriptTask/jstask19/jstask19.js
+++ BaiduIFE/SecondSection_JavaScriptTask/jstask19/jstask19.js
@@ -15,7 +15,7 @@
 	}
 	
 	function isDigit(value){
-		var pattern = /[1-9]\d*/g;  // 首位只能是1-9 \d 匹配任意数字0-9 *表示匹配0到n次
+		var pattern = /[1-9]\d*/g;  // 首位只能是1-9 \d 匹配任意数字0-9 *表示匹配0到n次 但是好像可以匹配09 类似的
 		if(pattern.test(value))
 		{
 			return true;
@@ -27,21 +27,75 @@
 	//function isInputE
 	$("left-in").onclick = function(){
 		var inputValue = document.getElementsByTagName("input")[0].value;
-		if(!isDigit(inputValue)){
-			alert("the input should be a num");
+		var numInputValue = parseInt(inputValue);
+		if(isNaN(numInputValue)){
+			alert("the input should be a number");
+		}else if(numInputValue < 10 || numInputValue > 100){
+			alert("please input the  num between 10 and 100")
 		}else if(data.length >= 60){
 			alert("the length of elements in the queue exceed 60");
 		}else{
 			data.unshift(inputValue);
 			renderQueue();
 		}
-		
+	};
+	// http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+	// just for testing/dev
+	function sleepFor( sleepDuration ){
+	    var now = new Date().getTime();
+	    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+	}
+	function bubbleSort(arr){
+
+		var flag = true;
+		for (var i = 0; i < arr.length-1; i++) {
+			for (var j = 0; j < arr.length-i-1; j++) {
+				if(arr[j]>arr[j+1])
+				{
+					flag=false;
+					var t = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = t;
+				}
+				sleepFor(2000);
+				renderQueue();
+			}
+			if (flag) {
+				break;
+			}
+		}
+	}
+	function bubbleSort(){
+
+		var flag = true;
+		for (var i = 0; i < data.length-1; i++) {
+			for (var j = 0; j < data.length-i-1; j++) {
+				if(data[j]>data[j+1])
+				{
+					flag=false;
+					var t = data[j];
+					data[j] = data[j+1];
+					data[j+1] = t;
+				}
+				renderQueue();
+				sleepFor(3000);
+				
+			}
+			if (flag) {
+				break;
+			}
+		}
+	}
+	$("bubble-sort").onclick=function(){
+		bubbleSort();
 	};
 	$("right-in").onclick = function(){
 		var inputValue = document.getElementsByTagName("input")[0].value;
-
-		if(!isDigit(inputValue)){
+		var numInputValue = parseInt(inputValue);
+		if(isNaN(numInputValue)){
 			alert("the input should be a number");
+		}else if(numInputValue < 10 || numInputValue > 100){
+			alert("please input the  num between 10 and 100")
 		}else if(data.length >= 60){
 			alert("the length of elements in the queue exceed 60");
 		}else{
@@ -71,7 +125,7 @@
 	};
 	function removeElement(e){
 		//var targetElement = arguments[0].target;
-		console.log(typeof e.dataset.num);
+		console.log(typeof e.dataset.num);   // string
 		console.log(e.getAttribute("data-num"));
 		data=data.filter(function(item,index,array){
 			return index!==parseInt(e.dataset.num);
@@ -82,7 +136,7 @@
 	function renderQueue(){
 		var html = "";
 		for(var i = 0;i < data.length;i++){
-			html += "<div onclick='removeElement(this);' style='float:left;background-color:red;margin:5px;' data-num='"+i+"'><span style='color:white;padding:5px;'>"+data[i]+"</span></div>";
+			html += "<div onclick='removeElement(this);' title='"+data[i]+"' style='float:left;margin-left:5px;margin-top:"+(100-data[i])+"px;background-color:red;width:20px;height:"+data[i]+"px;' data-num='"+i+"'></div>";
 		}
 		$("queue-wrap").innerHTML = html;
 	}
